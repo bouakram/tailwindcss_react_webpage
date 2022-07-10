0e2d47ce33d805e3c038c9db5c6aabb0072d8b34
Boughazi Akram <kikoua203@gmail.com>
2022-07-10 20:25:47 +0100

    complet the trusted components

---
+++ src/components/Trusted.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Trusted() {
+    return (
+        <div className=' bg-white flex flex-col items-center justify-center space-y-10 px-2 py-16'>
+            <div className='space-y-2 text-center'>
+            <h1 className='font-bold text-4xl'>Trusted by developers across the world</h1>
+            <p className='text-lg text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia nemo illo atque blanditiis cum ducimus doloribus enim rerum</p>
+            </div>
+            <ul className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2'>
+                <li className='cursor-pointer border rounded-lg p-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>100%</h3><p className='font-light text-sm text-gray-500'>completion</p></li>
+                <li className='cursor-pointer border rounded-lg p-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>24/27</h3><p className='font-light text-sm text-gray-500'>delevry</p></li>
+                <li className='cursor-pointer border rounded-lg p-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>100K</h3><p className='font-light text-sm text-gray-500'>transaction</p></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Trusted
