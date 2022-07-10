c41e5d5a370774cbd5d0de7ffade5727ff775e1a
Boughazi Akram <kikoua203@gmail.com>
2022-07-10 14:32:46 +0200

    adding tailwindcss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <h1 className='text-center font-bold text-blue-800'>this is a nav bar</h1>
+        </div>
+    )
+}
+
+export default Navbar
