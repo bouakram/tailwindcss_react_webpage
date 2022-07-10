4bbc5aa957e1e70fba5c0dfaa0f691b233c71037
Boughazi Akram <kikoua203@gmail.com>
2022-07-10 20:31:07 +0100

    adding some white spacing

--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -7,10 +7,10 @@ function Trusted() {
             <h1 className='font-bold text-4xl'>Trusted by developers across the world</h1>
             <p className='text-lg text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia nemo illo atque blanditiis cum ducimus doloribus enim rerum</p>
             </div>
-            <ul className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2'>
-                <li className='cursor-pointer border rounded-lg p-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>100%</h3><p className='font-light text-sm text-gray-500'>completion</p></li>
-                <li className='cursor-pointer border rounded-lg p-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>24/27</h3><p className='font-light text-sm text-gray-500'>delevry</p></li>
-                <li className='cursor-pointer border rounded-lg p-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>100K</h3><p className='font-light text-sm text-gray-500'>transaction</p></li>
+            <ul className='flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6'>
+                <li className='cursor-pointer border rounded-lg px-6 py-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>100%</h3><p className='font-light text-sm text-gray-500'>completion</p></li>
+                <li className='cursor-pointer border rounded-lg px-6 py-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>24/27</h3><p className='font-light text-sm text-gray-500'>delevry</p></li>
+                <li className='cursor-pointer border rounded-lg px-6 py-2 text-center shadow-lg'><h3 className='text-4xl font-bold text-indigo-400'>100K</h3><p className='font-light text-sm text-gray-500'>transaction</p></li>
             </ul>
         </div>
     )
