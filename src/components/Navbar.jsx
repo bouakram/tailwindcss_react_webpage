0fdd0801d8c3c351f5da8a74fdc11984af82983c
Boughazi Akram <kikoua203@gmail.com>
2022-07-10 14:45:46 +0200

    adding hero icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { BeakerIcon } from '@heroicons/react/solid'
 
 function Navbar() {
     return (
         <div>
-            <h1 className='text-center font-bold text-blue-800'>this is a nav bar</h1>
+            <h1 className='text-center font-bold text-blue-800'>this is a nav bar <BeakerIcon className='text-purple-400 w-4'/></h1>
         </div>
     )
 }
