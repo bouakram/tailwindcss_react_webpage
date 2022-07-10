96c05defe822f8e0b1c0b180afd115a7ec7eff7c
Boughazi Akram <kikoua203@gmail.com>
2022-07-10 16:15:33 +0200

    adding Hero components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {CloudUPLoadIcon,DatabaseIcon,PaperAiplaneIcon,ServerIcon} from '@heroicons/react/solid'
+import Bgimg from '../img/cyber-bg.png'
+
+function Hero() {
+    return (
+        <div></div>
+    )
+}
+
+export default Hero
