82fe9aedae689d5a98b98e6b7f571b3f87931749
Boughazi Akram <kikoua203@gmail.com>
2022-07-10 21:23:24 +0100

    adding support components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Trusted from "./components/Trusted";
+import Support from "./components/Support";
+
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Trusted />
+      <Support />
     </div>
   );
 }
