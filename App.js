0b2b21638fad892aa3e6ad166b2cedb194f4979e
Boughazi Akram <75098963+bouakram@users.noreply.github.com>
2022-07-13 02:20:42 +0100

    adding Allinone components

---
+++ App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Trusted from "./components/Trusted";
+import Support from "./components/Support";
+import AllInOne from "./components/AllInOne"
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Trusted />
+      <Support />
+      <AllInOne />
+    </div>
+  );
+}
+
+export default App;
