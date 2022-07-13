e4dc8392d363b0812089d89d3336c7e9097987b9
bouakram <kikoua203@gmail.com>
2022-07-13 22:42:44 +0100

    adding react-icons , adding two components -footer and pricing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Hero from "./components/Hero";
 import Trusted from "./components/Trusted";
 import Support from "./components/Support";
 import AllInOne from "./components/AllInOne"
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
@@ -11,6 +13,8 @@ function App() {
       <Trusted />
       <Support />
       <AllInOne />
+      <Pricing />
+      <Footer />
     </div>
   );
 }
